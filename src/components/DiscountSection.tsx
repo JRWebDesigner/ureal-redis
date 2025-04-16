299f8c8bda82c8204a25abe88e647b7449f865a8
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-16 15:46:19 -0400

    error

--- src/components/DiscountSection.tsx
+++ src/components/DiscountSection.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import Countdown from 'react-countdown'
+import Countdown, { CountdownRenderProps } from 'react-countdown'
 
 export default function DiscountSection() {
   const targetDate = new Date('2025-05-30T00:00:00')
 
-  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  const renderer = ({ days, hours, minutes, seconds, completed }: CountdownRenderProps) => {
     if (completed) {
       return <span>Oferta terminada!</span>
     } else {
@@ -38,7 +38,7 @@ export default function DiscountSection() {
 
   return (
     <section className="relative py-20 bg-[url('/Images/contador.jpg')] bg-cover">
-    <div className='bg-white w-full h-full absolute opacity-60 -my-20' />
+      <div className='bg-white w-full h-full absolute opacity-60 -my-20' />
       <div className="relative z-20 text-black container mx-auto px-4 flex gap-10 flex-wrap items-center justify-around">
         <motion.div
           initial={{ opacity: 0 }}
@@ -47,7 +47,7 @@ export default function DiscountSection() {
           transition={{ duration: 0.5 }}
           className="mb-12"
         >
-          <h2 className="text-4xl md:text-5xl font-bold mb-6">Regístrate ahora y obtén un <br/> 30% de descuento!</h2>
+          <h2 className="text-4xl md:text-5xl font-bold mb-6">Regístrate ahora y obtén un <br /> 30% de descuento!</h2>
           <p className="max-w-3xl mx-auto">
             No esperes más para mejorar tus habilidades y avanzar en tu carrera! Inscríbete en nuestros cursos de marketing hasta el 30 de mayo y obtén un 30% de descuento.
           </p>
